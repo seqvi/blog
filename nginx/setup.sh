e1dd4e74ed631c1fdc0fe2f4e5fd0804435a2c26
Feor <5939320+FeorPrime@users.noreply.github.com>
2024-07-28 13:53:47 +0300

    now ssl cert is maked by setup.sh

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -2,9 +2,19 @@
 
 echo "this script now will setup local development environment"
 
-rm -rf .configs
-rm -rf .data
-rm -rf .secrets
-mkdir -p .configs/nginx
-mkdir -p .data/logs
-mkdir -p .secrets
+configs='.configs'
+data='.data'
+secrets='.secrets'
+subj='/C=RU/ST=None/L=None/O=seqvi/CN=localhost.local'
+
+# clear folder structure
+rm -rf $configs
+rm -rf $data
+rm -rf $secrets
+# make new folder structure
+mkdir -p $configs/nginx
+mkdir -p $data/logs
+mkdir -p $secrets/nginx
+
+# produce new TLS cert and key for https connections
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $secrets/nginx/nginx-selfsigned.key -out $secrets/nginx/nginx-selfsigned.crt -subj $subj
